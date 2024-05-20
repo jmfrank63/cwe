14347c6718bf7d1c0de22000250667ca0cdec233
Johannes Maria Frank <jmfrank63@gmail.com>
2024-05-20 15:10:36 +0700

    postgres deployment minikube ok

---
+++ deployment/terraform/main.tf
@@ -0,0 +1,22 @@
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+resource "null_resource" "haproxy_config" {
+  provisioner "local-exec" {
+    command = "kubectl create configmap haproxy-config --from-file=/home/johannes/Projects/Rust/cwe/haproxy/haproxy.cfg --dry-run=client -o yaml | kubectl apply -f -"
+  }
+}
+
+module "postgres" {
+  source = "./modules/postgres"
+}
+
+# module "web_server" {
+#   source = "./modules/web_server"
+# }
+
+# module "haproxy" {
+#   source = "./modules/haproxy"
+#   depends_on = [null_resource.haproxy_config]
+# }
---
+++ deployment/terraform/modules/haproxy/main.tf
@@ -0,0 +1,94 @@
+resource "kubernetes_secret" "haproxy_certs" {
+  metadata {
+    name = "haproxy-certs"
+  }
+  data = {
+    "admin.pem" = filebase64("/home/johannes/.local/ssl/certs/cwe/admin.pem")
+  }
+}
+
+resource "kubernetes_deployment" "haproxy" {
+  metadata {
+    name = "haproxy"
+  }
+  spec {
+    replicas = 1
+    progress_deadline_seconds = 180
+    selector {
+      match_labels = {
+        app = "haproxy"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "haproxy"
+        }
+      }
+      spec {
+        container {
+          image = "haproxy"
+          name  = "haproxy"
+          port {
+            container_port = 443
+          }
+          port {
+            container_port = 8444
+          }
+          volume_mount {
+            mount_path = "/usr/local/etc/haproxy/certs"
+            name       = "haproxy-certs"
+          }
+          volume_mount {
+            mount_path = "/usr/local/etc/haproxy/haproxy.cfg"
+            sub_path   = "haproxy.cfg"
+            name       = "haproxy-config"
+          }
+        }
+        volume {
+          name = "haproxy-certs"
+          secret {
+            secret_name = kubernetes_secret.haproxy_certs.metadata.0.name
+          }
+        }
+        volume {
+          name = "haproxy-config"
+          config_map {
+            name = kubernetes_config_map.haproxy_config.metadata.0.name
+            items {
+              key  = "haproxy_cfg"
+              path = "haproxy.cfg"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "haproxy" {
+  metadata {
+    name = "haproxy"
+  }
+  spec {
+    selector = {
+      app = "haproxy"
+    }
+    port {
+      port        = 443
+      target_port = 443
+    }
+    port {
+      port        = 8444
+      target_port = 8444
+      name        = "admin"
+    }
+    type = "LoadBalancer"
+  }
+}
+
+resource "null_resource" "haproxy_config" {
+  provisioner "local-exec" {
+    command = "kubectl create configmap haproxy-config --from-file=/home/johannes/Projects/Rust/cwe/haproxy/haproxy.cfg --dry-run=client -o yaml | kubectl apply -f -"
+  }
+}
