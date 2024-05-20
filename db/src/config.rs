1461c5168309b68e1fd4f293292616e520ab3bab
Johannes Maria Frank <jmfrank63@gmail.com>
2024-05-20 09:11:14 +0700

    haproxy implemented

--- db/src/config.rs
+++ db/src/config.rs
@@ -10,18 +10,11 @@ pub struct DatabaseConfig {
 impl DatabaseConfig {
     pub fn new() -> Result<Self, Box<dyn std::error::Error>> {
         dotenv::dotenv().ok();
-        tracing::info!("Dotenv loaded");
         let host = dotenv::var("DB_HOST")?;
-        tracing::info!("DB_HOST: {}", host);
         let name = dotenv::var("DB_NAME")?;
-        tracing::info!("DB_NAME: {}", name);
         let user = dotenv::var("DB_USER")?;
-        tracing::info!("DB_USER: {}", user);
         let password = dotenv::var("DB_PASSWORD")?;
-        tracing::info!("DB_PASSWORD: [******]");
         let pool_size = dotenv::var("DB_POOL_SIZE")?.parse()?;
-        tracing::info!("DB_POOL_SIZE: {}", pool_size);
-
         Ok(Self { host, name, user, password, pool_size })
     }
 }
